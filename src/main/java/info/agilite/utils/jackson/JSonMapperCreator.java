3416b84d4cb4b6624ec6146a1865b174dae56651
Rafael Battazza <rafael@multitecsistemas.com.br>
2020-03-06 11:08:52 -0300

    ok

--- src/main/java/info/agilite/utils/jackson/JSonMapperCreator.java
+++ src/main/java/info/agilite/utils/jackson/JSonMapperCreator.java
@@ -4,7 +4,11 @@ package info.agilite.utils.jackson;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JSonMapperCreator {
 	private static ObjectMapper jsonMapper;
@@ -23,10 +27,13 @@ public class JSonMapperCreator {
 		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
 		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		objectMapper.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true);
 		objectMapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 		
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.registerModule(new Hibernate5Module());
+		objectMapper.registerModule(new JavaTimeModule());
 	}
-	
-	
 }
