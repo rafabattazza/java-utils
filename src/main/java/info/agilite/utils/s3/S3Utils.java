69d19294c3c60fb2cc72ff6ddad6294b20eea471
Rafael <rafael@multitecsistemas.com.br>
2020-11-25 14:46:37 -0300

    Upload

--- src/main/java/info/agilite/utils/s3/S3Utils.java
+++ src/main/java/info/agilite/utils/s3/S3Utils.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,7 +12,9 @@ import org.apache.http.HttpStatus;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
@@ -30,6 +33,11 @@ public class S3Utils {
 	public static void putFile(AmazonS3 s3, String buscketName, String key, File data) throws IOException {
 		s3.putObject(buscketName, key, new FileInputStream(data), null);
 	}
+	
+	public static void putInputStream(AmazonS3 s3, String buscketName, String key, InputStream is, ObjectMetadata metadata) throws IOException {
+		s3.putObject(buscketName, key, is, metadata);
+	}
+	
 	
 	public static List<String> listKeys(AmazonS3 s3, String buscketName, String prefix) throws IOException {
 		ObjectListing list = s3.listObjects(buscketName, prefix);
@@ -62,4 +70,9 @@ public class S3Utils {
 	public static void deleteObject(AmazonS3 s3, String buscketName, String key) throws IOException {
 		s3.deleteObject(buscketName, key);
 	}
+	public static void deleteObjects(AmazonS3 s3, String buscketName, List<String> keys) throws IOException {
+        DeleteObjectsRequest dor = new DeleteObjectsRequest(buscketName).withKeys(keys.toArray(new String[0]));
+        s3.deleteObjects(dor);
+	}
+
 }
