10aed8a58396b67c4da53afff5dcb5a74008a337
Rafael <rafael@multitecsistemas.com.br>
2021-03-16 15:00:24 -0300

    LogStatus

---
+++ src/main/java/info/agilite/utils/s3/LogStatus.java
@@ -0,0 +1,7 @@
+package info.agilite.utils.s3;
+
+public enum LogStatus {
+	OK,
+	ERRO,
+	FALHA;
+}
--- src/main/java/info/agilite/utils/s3/LogsToS3.java
+++ src/main/java/info/agilite/utils/s3/LogsToS3.java
@@ -10,9 +10,9 @@ import info.agilite.utils.StringUtils;
 
 public class LogsToS3 {
 
-	public static void enviarLog(AmazonS3 s3, String rastreamento, String pid, String schema, String sistema) throws IOException {
+	public static void enviarLog(AmazonS3 s3, String rastreamento, String pid, String schema, String sistema, LogStatus status) throws IOException {
 		String data = DateUtils.formatDate(LocalDate.now(), "yyMMdd");
-		String key = StringUtils.concat("LOGS/", data, "/", schema, "/", sistema, "/", pid, ".log");
+		String key = StringUtils.concat("LOGS/", data, "/", schema, "/", sistema, "/", pid, "_", status.name(), ".log");
 		S3Utils.putString(s3, "agilite-files", key, rastreamento);
 	}
 }
